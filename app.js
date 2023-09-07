76d1b09b9e10560c631cb3c6db22417337657aa2
Jonna <jonna.piironen@gmail.com>
2023-09-07 17:29:37 +0300

    commit

---
+++ app.js
@@ -0,0 +1,16 @@
+import { serve } from "./deps.js";
+import { configure, renderFile } from "./deps.js";
+
+const handleRequest = async (request) => {
+    const url = new URL(request.url);
+    if (url.pathname === "/count") {
+        data.count++;
+        return new Response(await renderFile("count.eta", data), responseDetails);
+      } else if (url.pathname === "/meaning"){
+        return new Response("Seeking truths beyond meaning of life, you will find 43.");
+    } else {
+        return new Response("Nothing here yet.");
+    }
+};
+
+serve(handleRequest, { port: 7777 });
